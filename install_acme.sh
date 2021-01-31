558a6057d739d2a9d3e9c4b3cd4cf66443f0b3bf
plentymore <plentymorea@gmail.com>
2021-01-31 16:59:29 +0800

    init

---
+++ install_acme.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/acmesh-official/acme.sh.git
+cd ./acme.sh
+./acme.sh --install
+
+sudo apt-get install socat
+
+acme.sh --issue -d area.example.com -d cdnarea.example.com -w /home/{user}/V2RAYWSS
+
+
+acme.sh --install-cert -d area.example.com \
+--key-file       /home/{user}/V2RAYWSS/config/autossl.key  \
+--fullchain-file /home/{user}/V2RAYWSS/config/autossl.pem \
+--reloadcmd     "docker restart nginx"
+
+
